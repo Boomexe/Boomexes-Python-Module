99c58771ec5d02bf7f51a9101f5075a9e81bf0ea
Boomexe <97500733+Boomexe@users.noreply.github.com>
2022-05-07 17:45:57 -0700

    commit

---
+++ src/boomexe/Boomexe.py
@@ -0,0 +1,10 @@
+import random
+
+def say_hello(name):
+    return f'Hello, {name}!'
+
+def add_numbers(num1, numb2):
+    return num1 + numb2
+
+def return_random_chance(a, b):
+    return random.randint(a, b) == a
--- src/main_package/module.py
+++
@@ -1,5 +0,0 @@
-def say_hello(name):
-    return f'Hello, {name}!'
-
-def add_numbers(num1, numb2):
-    return num1 + numb2
